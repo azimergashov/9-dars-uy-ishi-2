334a9f3403ca9fd3394503963619a8a6bf0d2048
azimergashov <azimboy.ergashov.1@gmail.com>
2022-06-01 08:13:26 +0500

    finish

---
+++ js/index.js
@@ -0,0 +1,17 @@
+const summa = Number(prompt("summani kiriting"))
+
+const calcSumma = function(arr){
+    const s= []
+
+    if(summa>50 && summa<300 ){
+        const sum= summa + summa /100 * 15
+        s.push(sum)
+    }else{
+        const  a= summa +summa/100 *20
+        s.push(a)
+    }
+
+    return s
+}
+
+console.log(calcSumma(summa));
